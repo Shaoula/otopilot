20441918a95b2652dc84ec2a36241ec2f67e51c1
demhat <mail.demhatsahin@gmail.com>
2025-01-18 16:10:49 +0300

    Feat: Init

---
+++ app/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'sky',
+      neutral: 'neutral',
+    },
+    button: {
+      defaultVariants: {
+        // Set default button color to neutral
+        // color: 'neutral'
+      }
+    }
+  }
+})
